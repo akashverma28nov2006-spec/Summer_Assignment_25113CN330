1e9b76171425403fff6df78daf8a80add6833250
Akash verma <akashverma28nov2006@gmail.com>
2026-06-05 21:26:47 +0530

    Added Day 4 program

--- summer assignment/DAY 5/factors.c
+++ summer assignment/DAY 5/factors.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
+
 int main() {
     int n, i;
 
     printf("Enter a number: ");
     scanf("%d", &n);
 
-    for(i = 1; i <= 10; i++) {
-        printf("%d x %d = %d\n", n, i, n * i);
+    printf("Factors are: ");
+
+    for(i = 1; i <= n; i++) {
+        if(n % i == 0)
+            printf("%d ", i);
     }
 
     return 0;
